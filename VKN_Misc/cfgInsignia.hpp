9651de2b4b67486d0298bd488932276eedc730b1
56curious <41159450+56curious@users.noreply.github.com>
2019-04-28 20:34:48 +0100

    Fixed formatting

--- VKN_Misc/cfgInsignia.hpp
+++ VKN_Misc/cfgInsignia.hpp
@@ -1,55 +1,45 @@
-class CfgUnitInsignia
-{
-	class VikingPMC
-	{
+class CfgUnitInsignia {
+	class VikingPMC	{
 		displayName = "[VKN] Viking PMC";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\ORBATLogo.paa";
 	};
-	class Aegir
-	{
+	class Aegir	{
 		displayName = "[VKN] Aegir";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\Agier.paa";
 	};
-	class Balder
-	{
+	class Balder {
 		displayName = "[VKN] Balder";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\Balder.paa";
 	};
-	class Fenrir
-	{
+	class Fenrir {
 		displayName = "[VKN] Fenrir";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\Fenrir.paa";
 	};
-	class Loki
-	{
+	class Loki {
 		displayName = "[VKN] Loki";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\Loki.paa";
 	};
-	class Odin
-	{
+	class Odin {
 		displayName = "[VKN] Odin";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\Odin.paa";
 	};
-	class Thor
-	{
+	class Thor {
 		displayName = "[VKN] Thor";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\Thor.paa";
 	};
-	class Valkyrie
-	{
+	class Valkyrie {
 		displayName = "[VKN] Valkyrie";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\Valkyrie.paa";
 	};
-	class VIS
-	{
+	class VIS {
 		displayName = "[VKN] VIS";
 		author = "Viking PMC";
 		texture = "\VKN_Misc\markers\data\VIS.paa";
