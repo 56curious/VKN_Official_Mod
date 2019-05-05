aa30ff2eb5bb5489c516b51a72b28182242e88af
56curious <41159450+56curious@users.noreply.github.com>
2019-05-05 22:37:03 +0100

    Removed facewear from config.cpp and moved to sep file.

--- VKN_Gear/Config.cpp
+++ VKN_Gear/Config.cpp
@@ -31,6 +31,7 @@ class cfgVehicles {
 	class B_Kitbag_rgr;
 	class B_Soldier_F;
 	class B_GEN_Soldier_F;
+  class B_Soldier_base_F;
 	class B_Carryall_Base;
 	class B_AssaultPack_Base;
 	class B_Soldier_lite_F;
@@ -39,6 +40,7 @@ class cfgVehicles {
  	class O_V_Soldier_Viper_F;
 	class O_Soldier_F;
 	class C_Story_Mechanic_01_F;
+  
 	class ReammoBox_F;
 	class ReammoBox;
 	class NATO_Box_Base;
@@ -70,14 +72,14 @@ class cfgWeapons {
 	class ItemInfo;
 	class H_HelmetIA;
 	class H_HelmetB;
-    class H_Cap_red;
-    class H_Beret_02;
-    class H_helmetO_ViperSP_hex_F;
-    class U_B_CTRG_Soldier_F;
+  class H_Cap_red;
+  class H_Beret_02;
+  class H_helmetO_ViperSP_hex_F;
+  class U_B_CTRG_Soldier_F;
 
-    class Vest_NoCamo_Base;
-    class Vest_Camo_Base : ItemCore {
-		class ItemInfo;
+  class Vest_NoCamo_Base;
+  class Vest_Camo_Base : ItemCore {
+	class ItemInfo;
 	};
 
     #include "\VKN_Gear\cfgWeapons\headgear.hpp"
@@ -88,98 +90,8 @@ class cfgWeapons {
 
 
 class cfgGlasses {
-	class G_Balaclava_TI_blk_F;
-	class VKN_Stealth_Balaclava_MTP: G_Balaclava_TI_blk_F
-	{
-		scope=2;
-		author="Apple";
-		_generalMacro="G_Balaclava_lowprofile";
-		displayName="[VKN] FBA Stealth Balaclava (MTP)";
-		hiddenSelections[]=
-		{
-			"camo"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\VKN_Gear\data\facewear\VKN_S_Balaclava_MTP.paa"
-		};
-	};
-	class VKN_Stealth_Balaclava_BLK: G_Balaclava_TI_blk_F
-	{
-		author="Apple";
-		displayName="[VKN] FBA Stealth Balaclava + Goggles (Black)";
-		model="\A3\Characters_F_Exp\BLUFOR\G_Balaclava_TI_G_F.p3d";
-		hiddenSelections[]=
-		{
-			"camo1",
-			"camo2"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\VKN_Gear\data\facewear\VKN_S_Balaclava_BLK.paa",
-			"\VKN_Gear\data\facewear\VKN_Combat_Goggles.paa"
-		};
-	};
-	class VKN_Stealth_Balaclava_SNOW: G_Balaclava_TI_blk_F
-	{
-		scope=2;
-		author="Apple";
-		_generalMacro="G_Balaclava_lowprofile";
-		displayName="[VKN] FWA Stealth Balaclava (SNOW)";
-		hiddenSelections[]=
-		{
-			"camo"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW.paa"
-		};
-	};
-	class VKN_Stealth_Balaclava_SNOW_CAMO: G_Balaclava_TI_blk_F
-	{
-		scope=2;
-		author="Apple";
-		_generalMacro="G_Balaclava_lowprofile";
-		displayName="[VKN] FWA Stealth Balaclava (SNOW CAMO)";
-		hiddenSelections[]=
-		{
-			"camo"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW_CAMO.paa"
-		};
-	};
-	class VKN_Stealth_Balaclava_SNOW_G: G_Balaclava_TI_blk_F
-	{
-		author="Apple";
-		displayName="[VKN] FWA Stealth Balaclava + Goggles (SNOW)";
-		model="\A3\Characters_F_Exp\BLUFOR\G_Balaclava_TI_G_F.p3d";
-		hiddenSelections[]=
-		{
-			"camo1",
-			"camo2"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW.paa",
-			"\VKN_Gear\data\facewear\VKN_Combat_Goggles.paa"
-		};
-	};
-	class VKN_Stealth_Balaclava_SNOW_CAMO_G: G_Balaclava_TI_blk_F
-	{
-		author="Apple";
-		displayName="[VKN] FWA Stealth Balaclava + Goggles (SNOW CAMO)";
-		model="\A3\Characters_F_Exp\BLUFOR\G_Balaclava_TI_G_F.p3d";
-		hiddenSelections[]=
-		{
-			"camo1",
-			"camo2"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW_CAMO.paa",
-			"\VKN_Gear\data\facewear\VKN_Combat_Goggles.paa"
-		};
-	};
+
+  class G_Balaclava_TI_blk_F;
+
+  #include "\VKN_Gear\cfgGlasses\Facewear.hpp"
 };
