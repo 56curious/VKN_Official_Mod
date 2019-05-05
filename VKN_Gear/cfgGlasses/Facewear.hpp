06eac7688e7711a06a766b9457fa7630403ebdeb
56curious <41159450+56curious@users.noreply.github.com>
2019-05-05 22:34:49 +0100

    Added Facewear file for all facewear.

---
+++ VKN_Gear/cfgGlasses/Facewear.hpp
@@ -0,0 +1,51 @@
+class VKN_Stealth_Balaclava_MTP: G_Balaclava_TI_blk_F {
+  scope=2;
+  author="Apple";
+  _generalMacro="G_Balaclava_lowprofile";
+  displayName="[VKN] FBA Stealth Balaclava (MTP)";
+  picture="\VKN_Misc\VKN_Logo_Inverted.paa";
+  hiddenSelections[]={"camo"};
+  hiddenSelectionsTextures[]={"\VKN_Gear\data\facewear\VKN_S_Balaclava_MTP.paa"};
+};
+class VKN_Stealth_Balaclava_BLK: G_Balaclava_TI_blk_F {
+  author="Apple";
+  displayName="[VKN] FBA Stealth Balaclava + Goggles (Black)";
+  picture="\VKN_Misc\VKN_Logo_Inverted.paa";
+  model="\A3\Characters_F_Exp\BLUFOR\G_Balaclava_TI_G_F.p3d";
+  hiddenSelections[]={"camo1","camo2"};
+  hiddenSelectionsTextures[]={"\VKN_Gear\data\facewear\VKN_S_Balaclava_BLK.paa", "\VKN_Gear\data\facewear\VKN_Combat_Goggles.paa"};
+};
+class VKN_Stealth_Balaclava_SNOW: G_Balaclava_TI_blk_F {
+  scope=2;
+  author="Apple";
+  _generalMacro="G_Balaclava_lowprofile";
+  displayName="[VKN] FWA Stealth Balaclava (SNOW)";
+  picture="\VKN_Misc\VKN_Logo_Inverted.paa";
+  hiddenSelections[]={"camo"};
+  hiddenSelectionsTextures[]={"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW.paa"};
+};
+class VKN_Stealth_Balaclava_SNOW_CAMO: G_Balaclava_TI_blk_F {
+  scope=2;
+  author="Apple";
+  _generalMacro="G_Balaclava_lowprofile";
+  displayName="[VKN] FWA Stealth Balaclava (SNOW CAMO)";
+  picture="\VKN_Misc\VKN_Logo_Inverted.paa";
+  hiddenSelections[]={"camo"};
+  hiddenSelectionsTextures[]={"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW_CAMO.paa"};
+};
+class VKN_Stealth_Balaclava_SNOW_G: G_Balaclava_TI_blk_F {
+  author="Apple";
+  displayName="[VKN] FWA Stealth Balaclava + Goggles (SNOW)";
+  picture="\VKN_Misc\VKN_Logo_Inverted.paa";
+  model="\A3\Characters_F_Exp\BLUFOR\G_Balaclava_TI_G_F.p3d";
+  hiddenSelections[]={"camo1","camo2"};
+  hiddenSelectionsTextures[]={"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW.paa", "\VKN_Gear\data\facewear\VKN_Combat_Goggles.paa"};
+};
+class VKN_Stealth_Balaclava_SNOW_CAMO_G: G_Balaclava_TI_blk_F {
+  author="Apple";
+  displayName="[VKN] FWA Stealth Balaclava + Goggles (SNOW CAMO)";
+  picture="\VKN_Misc\VKN_Logo_Inverted.paa";
+  model="\A3\Characters_F_Exp\BLUFOR\G_Balaclava_TI_G_F.p3d";
+  hiddenSelections[]={"camo1","camo2"};
+  hiddenSelectionsTextures[]={"\VKN_Gear\data\facewear\VKN_S_Balaclava_SNOW_CAMO.paa", "\VKN_Gear\data\facewear\VKN_Combat_Goggles.paa"};
+};
