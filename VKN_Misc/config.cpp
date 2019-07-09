fe7e1397bcc1a19f455cc1743c32f13701f2e131
56curious <41159450+56curious@users.noreply.github.com>
2019-07-09 22:17:03 +0100

    Adjusted configs and paths

--- VKN_Misc/config.cpp
+++ VKN_Misc/config.cpp
@@ -34,103 +34,17 @@ enableDebugConsole[] = {"76561197996326460", "76561198116251840"};
 
 //Orbats and mainMenu Credit
 class cfgOrbat {
-    #include "VKN_Orbat.hpp"
+    #include "configs\VKN_Orbat.hpp"
 };
 
 class cfgCredits {
-    #include "VKN_Orbat.hpp"
+    #include "configs\VKN_Orbat.hpp"
 
     class VKNCreditsMovie {
-
-        item1[] = {
-            {
-                "Mod Developers"
-            },
-            {
-                "Curious - Project Leader (Functions, Configs, Model Maker)",
-                "Jonmo (Texture and Configs)",
-                "Eric Mchogan (Textures and Configs)",
-                "Max Kupleoni (Textures)",
-                "Poxxl (Textures)",
-                "Apple (Textures and Configs)",
-                "JeriCrunch (Textures and Materials)"
-            }
-        };
-        item2[] = {
-            {
-                "Head's of Departments"
-            },
-            {
-                "Bane - Founder / Head of Personnel (Recruitment)",
-                "Skywalker / Lion - Head of Operations (Training)",
-                "Havok - Head of Tech",
-                "Burkal - Head of Media"
-            }
-        };
-        item3[] = {
-            {
-                "Personnel"
-            },
-            {
-                "Luke",
-                "Bane",
-                "ragingpiano",
-                "Shagger",
-                "Maxy"
-            }
-        };
-        item4[] = {
-            {
-                "Operations"
-            },
-            {
-                "Jonmo",
-                "Curious",
-                "DocRoe",
-                "J. Bryan",
-                "LiftsHeavyThings",
-                "Skywalker",
-                "Theodore",
-                "The_Apothecary",
-                "Shagger",
-                "Lion",
-                "Wolfy",
-                "Happy",
-                "Tom",
-                "Ryan",
-                "Garnet",
-                "Supr3mo",
-                "Sir Robert Pierre"
-            }
-        };
-        item5[] = {
-            {
-                "Media"
-            },
-            {
-                "Curious",
-                "Burkal",
-                "Sir Robert Piérre",
-                "aryay",
-                "PoxxI",
-                "Max Kupleoni"
-            }
-        };
-        item6[] = {
-            {
-                "Tech"
-            },
-            {
-                "DJ_ToyBoy",
-                "Curious",
-                "Havok",
-                "Alliaz",
-                "TheGeneral",
-                "Winters",
-                "KP",
-                "Apple"
-            }
-        };
+      #include "configs\cfgCredits_Roles.hpp"
+    };
+    class VKNCreditsRanks {
+      #include "configs\cfgCredits_Ranks.hpp"
     };
 };
 
@@ -145,7 +59,7 @@ class CfgEditorSubcategories {
 #include "fonts\cfgFontFamilies.hpp"
 #include "markers\cfgMarkers.hpp"
 #include "compositions\cfgGroups.hpp"
-#include "cfgInsignia.hpp"
+#include "configs\cfgInsignia.hpp"
 
 
 class RscStandardDisplay;
