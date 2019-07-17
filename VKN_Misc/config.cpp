de90838137885d02440f8def347e105d9dcb4323
56curious <41159450+56curious@users.noreply.github.com>
2019-07-17 11:10:12 +0100

    Increased the speed at which you climb a ladder.

--- VKN_Misc/config.cpp
+++ VKN_Misc/config.cpp
@@ -55,6 +55,22 @@ class CfgEditorSubcategories {
     };
 };
 
+class CfgMovesBasic;
+class CfgMovesMaleSdr: CfgMovesBasic
+{
+	class States
+	{
+		class LadderCivilStatic;
+		class LadderCivilUpLoop: LadderCivilStatic
+		{
+			speed="1.05/(2/3)";
+		};
+	};
+};
+class CfgAnimation
+{
+	ladderSpeed="0.5*2";
+};
 
 #include "fonts\cfgFontFamilies.hpp"
 #include "markers\cfgMarkers.hpp"
