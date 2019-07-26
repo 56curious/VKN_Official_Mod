ab807e5d8f32f895dcde2cf1162e7beb496fbb0f
56curious <41159450+56curious@users.noreply.github.com>
2019-07-26 01:29:14 +0100

    Update removeClasses.hpp

--- VKN_Objects/Vehicles/removeClasses.hpp
+++ VKN_Objects/Vehicles/removeClasses.hpp
@@ -1,26 +1,53 @@
-class GeK_TLC100: Car_F {scope = 1;};
-class revo_tlc_civ: Car_F {scope = 1;};
-class Mrshounka_raptor_base: Car_F {scope = 1;};
-class Mrshounka_raptor2_civ: Car_F {scope = 1;};
-class Mrshounka_raptor2_noir: Car_F {scope = 1;};
-class Mrshounka_raptor2_bleufonce: Car_F {scope = 1;};
-class Mrshounka_raptor2_rouge: Car_F {scope = 1;};
-class Mrshounka_raptor2_jaune: Car_F {scope = 1;};
-class Mrshounka_raptor2_rose: Car_F {scope = 1;};
-class Mrshounka_raptor2_grise: Car_F {scope = 1;};
-class Mrshounka_raptor2_violet: Car_F {scope = 1;};
-class Mrshounka_raptor2_orange: Car_F {scope = 1;};
-class Mrshounka_raptor2_Desert: Car_F {scope = 1;};
-class Mrshounka_raptor2_Woodl: Car_F {scope = 1;};
-class Mrshounka_raptor2_Patriot: Car_F {scope = 1;};
-class Mrshounka_raptor2_JP: Car_F {scope = 1;};
-class shounka_a3_brinks_banque_base: Car_F {scope = 1;};
-class shounka_a3_brinks: Car_F {scope = 1;};
-class shounka_a3_brinks_noir: Car_F {scope = 1;};
-class shounka_a3_brinks_bleufonce: Car_F {scope = 1;};
-class shounka_a3_brinks_rouge: Car_F {scope = 1;};
-class shounka_a3_brinks_jaune: Car_F {scope = 1;};
-class shounka_a3_brinks_rose: Car_F {scope = 1;};
-class shounka_a3_brinks_grise: Car_F {scope = 1;};
-class shounka_a3_brinks_violet: Car_F {scope = 1;};
-class shounka_a3_brinks_orange: Car_F {scope = 1;};
+/*
+class GeK_TLC100: Car_F {scope = 0;};
+class revo_tlc_civ: Car_F {scope = 0;};
+class Mrshounka_raptor_base: Car_F {scope = 0;};
+class Mrshounka_raptor2_civ: Car_F {scope = 0;};
+class Mrshounka_raptor2_noir: Car_F {scope = 0;};
+class Mrshounka_raptor2_bleufonce: Car_F {scope = 0;};
+class Mrshounka_raptor2_rouge: Car_F {scope = 0;};
+class Mrshounka_raptor2_jaune: Car_F {scope = 0;};
+class Mrshounka_raptor2_rose: Car_F {scope = 0;};
+class Mrshounka_raptor2_grise: Car_F {scope = 0;};
+class Mrshounka_raptor2_violet: Car_F {scope = 0;};
+class Mrshounka_raptor2_orange: Car_F {scope = 0;};
+class Mrshounka_raptor2_Desert: Car_F {scope = 0;};
+class Mrshounka_raptor2_Woodl: Car_F {scope = 0;};
+class Mrshounka_raptor2_Patriot: Car_F {scope = 0;};
+class Mrshounka_raptor2_JP: Car_F {scope = 0;};
+class shounka_a3_brinks_banque_base: Car_F {scope = 0;};
+class shounka_a3_brinks: Car_F {scope = 0;};
+class shounka_a3_brinks_noir: Car_F {scope = 0;};
+class shounka_a3_brinks_bleufonce: Car_F {scope = 0;};
+class shounka_a3_brinks_rouge: Car_F {scope = 0;};
+class shounka_a3_brinks_jaune: Car_F {scope = 0;};
+class shounka_a3_brinks_rose: Car_F {scope = 0;};
+class shounka_a3_brinks_grise: Car_F {scope = 0;};
+class shounka_a3_brinks_violet: Car_F {scope = 0;};
+class shounka_a3_brinks_orange: Car_F {scope = 0;};
+*/
+
+//class EC135Unarmedlu; //Unarmed with camera
+//class EC135Armedlu; //Armed with camera
+//class EC135Noirlu; //Unarmed but no camera
+//class EC135Rlu; //Unarmed Basic Medical
+//class EC135SAlu; //Unarmed Medical Alternative
+
+class EC135Rlu: Helicopter_Base_F { scope = 0; };
+class EC135ADlu: Helicopter_Base_F { scope = 0; };
+class EC135SClu: Helicopter_Base_F { scope = 0; };
+class EC135SAlu: Helicopter_Base_F { scope = 0; };
+class EC135ANlu: Helicopter_Base_F { scope = 0; };
+class EC135GNlu: Helicopter_Base_F { scope = 0; };
+class EC135SARlu: Helicopter_Base_F { scope = 0; };
+class EC135Unarmedlu: Helicopter_Base_F { scope = 0; };
+class EC135Armedlu: Helicopter_Base_F { scope = 0; };
+class EC135Noirlu: Helicopter_Base_F { scope = 0; };
+class EC135Blanclu: Helicopter_Base_F { scope = 0; };
+class EC135Grislu: Helicopter_Base_F { scope = 0; };
+class EC135Bleulu: Helicopter_Base_F { scope = 0; };
+class EC135Rougelu: Helicopter_Base_F { scope = 0; };
+class EC135Jaunelu: Helicopter_Base_F { scope = 0; };
+class EC135Orangelu: Helicopter_Base_F { scope = 0; };
+class EC135Vertlu: Helicopter_Base_F { scope = 0; };
+class EC135Roselu: Helicopter_Base_F { scope = 0; };
