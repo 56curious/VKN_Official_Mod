6386276fd3c8db78292651d5642a2ec0afc7eac1
56curious <41159450+56curious@users.noreply.github.com>
2020-05-05 16:19:10 +0100

    fixed audio not playing

--- VKN_Objects/Configs/AS_365_565.hpp
+++ VKN_Objects/Configs/AS_365_565.hpp
@@ -1,17 +1,17 @@
 class AS_365: AS_365_base {
   scope = 0;
-  soundEngineOnInt[]={"\VKN_Objects\Vehicles\as_365\data\Startup_Int.ogg",0.31622776,1};
-  soundEngineOnExt[]={"\VKN_Objects\Vehicles\as_365\data\Startup_Ext.ogg",2.2387211,1,600};
+  soundEngineOnInt[]={"\VKN_Objects\Vehicles\as_365\data\Startup_Int",0.31622776,1};
+  soundEngineOnExt[]={"\VKN_Objects\Vehicles\as_365\data\Startup_Ext",2.2387211,1,600};
   soundEngineOffInt[]={"A3\Sounds_F\vehicles\air\Heli_Transport_02\Heli_Transport_02_int_stop_v2",	0.31622776,1};
   soundEngineOffExt[]={"A3\Sounds_F\vehicles\air\Heli_Transport_02\Heli_Transport_02_ext_stop_v2",2.2387211,	1,600};
   class Sounds {
     class EngineExt {
-      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext.ogg",1.2589254,1,900};
+      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext",1.2589254,1,900};
       frequency="rotorSpeed";
       volume="camPos*(rotorSpeed-0.72)*4";
     };
     class RotorExt {
-      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext.ogg",1.7782794,1,2000};
+      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext",1.7782794,1,2000};
       frequency="rotorSpeed * rotorSpeed * (1 - rotorThrust/6)";
       volume="camPos*(0 max (rotorSpeed-0.1))*(1 + rotorThrust)";
       cone[]={1.6,3.1400001,1.6,0.94999999};
@@ -23,12 +23,12 @@ class AS_365: AS_365_base {
       cone[]={0.69999999,1.3,1,0};
     };
     class EngineInt {
-      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int.ogg",1,	1};
+      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int",1,	1};
       frequency="rotorSpeed";
       volume="(1-camPos)*(rotorSpeed-0.75)*4";
     };
     class RotorInt {
-      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int.ogg",1.2589254,1};
+      sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int",1.2589254,1};
       frequency="rotorSpeed * rotorSpeed * (1 - rotorThrust/6)";
       volume="(1-camPos)*(0 max (rotorSpeed-0.1))*(1 + rotorThrust)";
     };
@@ -38,12 +38,12 @@ class AS_365: AS_365_base {
     class SoundEvents {};
     class Sounds {
       class EngineExt {
-        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext.ogg",1.2589254,1,900	};
+        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext",1.2589254,1,900	};
         frequency="rotorSpeed";
         volume="camPos*(rotorSpeed-0.72)*4";
       };
       class RotorExt {
-        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext.ogg",1.7782794,1,2000};
+        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_ext",1.7782794,1,2000};
         frequency="rotorSpeed * rotorSpeed * (1 - rotorThrust/6)";
         volume="camPos*(0 max (rotorSpeed-0.1))*(1 + rotorThrust)";
         cone[]={1.6,3.1400001,1.6,0.94999999};
@@ -55,12 +55,12 @@ class AS_365: AS_365_base {
         cone[]={0.69999999,1.3,1,0};
       };
       class EngineInt {
-        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int.ogg",	1,	1};
+        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int",	1,	1};
         frequency="rotorSpeed";
         volume="(1-camPos)*(rotorSpeed-0.75)*4";
       };
       class RotorInt {
-        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int.ogg",	1.2589254,	1};
+        sound[]={"\VKN_Objects\Vehicles\as_365\data\EngineLoop_int",	1.2589254,	1};
         frequency="rotorSpeed * rotorSpeed * (1 - rotorThrust/6)";
         volume="(1-camPos)*(0 max (rotorSpeed-0.1))*(1 + rotorThrust)";
       };
