acdf3b9f08fe30ec8d46131eb5f1027f1be58bdc
56curious <41159450+56curious@users.noreply.github.com>
2020-12-18 13:12:44 +0000

    Fixed Zeus assignment issues (Blackscreen)

--- VKN_Misc/configs/VKN_Template_Tool_Info.hpp
+++ VKN_Misc/configs/VKN_Template_Tool_Info.hpp
@@ -7,18 +7,18 @@ class VKN_Template_Tool_Info {
     {
     	idc = 2200;
 
-    	x = 0.267911 * safezoneW + safezoneX;
-    	y = 0.225 * safezoneH + safezoneY;
-    	w = 0.464179 * safezoneW;
-    	h = 0.539 * safezoneH;
+    	x = 0.273068 * safezoneW + safezoneX;
+    	y = 0.192 * safezoneH + safezoneY;
+    	w = 0.459021 * safezoneW;
+    	h = 0.583 * safezoneH;
     	colorBackground[] = {0.137,0.137,0.137,0.75};
     };
     class BackgroundLight: IGUIBack
     {
     	idc = 2201;
 
-    	x = 0.267911 * safezoneW + safezoneX;
-    	y = 0.225 * safezoneH + safezoneY;
+    	x = 0.273068 * safezoneW + safezoneX;
+    	y = 0.192 * safezoneH + safezoneY;
     	w = 0.464179 * safezoneW;
     	h = 0.044 * safezoneH;
     	colorBackground[] = {0.03,0.51,1,0.75};
@@ -28,18 +28,19 @@ class VKN_Template_Tool_Info {
     	idc = 1000;
 
     	text = "VKN Template Tool Information"; //--- ToDo: Localize;
-    	x = 0.267911 * safezoneW + safezoneX;
-    	y = 0.225 * safezoneH + safezoneY;
+    	x = 0.273068 * safezoneW + safezoneX;
+    	y = 0.192 * safezoneH + safezoneY;
     	w = 0.361028 * safezoneW;
     	h = 0.044 * safezoneH;
     };
     class Frame: RscFrame
     {
     	idc = 1800;
+
     	x = 0.283383 * safezoneW + safezoneX;
-    	y = 0.324 * safezoneH + safezoneY;
+    	y = 0.269 * safezoneH + safezoneY;
     	w = 0.27335 * safezoneW;
-    	h = 0.385 * safezoneH;
+    	h = 0.484 * safezoneH;
     };
     class Explain: RscStructuredText
     {
@@ -47,9 +48,9 @@ class VKN_Template_Tool_Info {
 
     	text = "Due to the way that arma 3 works, I currently cannot do this next part for you :/ I am working on an extension that does it all, but until then you will need to do it.<br />You'll need to follow these steps carefully to ensure the mission will work properly. You can leave this box open and still use the rest of the editor's menus.<br /><br />Firstly, ensure you have saved the mission and named it.<br />Second, goto Scenario >> Open Scenario Folder<br />Create a new text document and name it description.ext<br />Click the 'Copy File Contents' button below.<br /><br />Do the same for the rest of the files on the right hand side of this menu.<br />Make sure you have set the file format for each file!!!<br /> <br />All done, Zeus will work with virtual entities. The spectator settings will work, loadouts will save and dynamic groups will be setup!"; //--- ToDo: Localize;
     	x = 0.283383 * safezoneW + safezoneX;
-    	y = 0.324 * safezoneH + safezoneY;
+    	y = 0.269 * safezoneH + safezoneY;
     	w = 0.27335 * safezoneW;
-    	h = 0.385 * safezoneH;
+    	h = 0.484 * safezoneH;
     };
     class DescTitle: RscText
     {
@@ -57,7 +58,7 @@ class VKN_Template_Tool_Info {
 
     	text = "description.ext"; //--- ToDo: Localize;
     	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.302 * safezoneH + safezoneY;
+    	y = 0.269 * safezoneH + safezoneY;
     	w = 0.0979933 * safezoneW;
     	h = 0.022 * safezoneH;
     };
@@ -68,7 +69,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Name"; //--- ToDo: Localize;
     	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.335 * safezoneH + safezoneY;
+    	y = 0.302 * safezoneH + safezoneY;
     	w = 0.067048 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -80,7 +81,41 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Contents"; //--- ToDo: Localize;
     	x = 0.654726 * safezoneW + safezoneX;
-    	y = 0.335 * safezoneH + safezoneY;
+    	y = 0.302 * safezoneH + safezoneY;
+    	w = 0.0722056 * safezoneW;
+    	h = 0.033 * safezoneH;
+    	colorActive[] = {0.03,0.51,1,0.75};
+    };
+    class LocalServerText: RscText
+    {
+    	idc = 1002;
+
+    	text = "initPlayerServer.sqf"; //--- ToDo: Localize;
+    	x = 0.572206 * safezoneW + safezoneX;
+    	y = 0.368 * safezoneH + safezoneY;
+    	w = 0.0979933 * safezoneW;
+    	h = 0.022 * safezoneH;
+    };
+    class LocalServerFileName: RscButton
+    {
+    	idc = 1600;
+    	action = "copyToClipboard ""initPlayerServer.sqf""";
+
+    	text = "Copy File Name"; //--- ToDo: Localize;
+    	x = 0.572206 * safezoneW + safezoneX;
+    	y = 0.401 * safezoneH + safezoneY;
+    	w = 0.067048 * safezoneW;
+    	h = 0.033 * safezoneH;
+    	colorActive[] = {0.03,0.51,1,0.75};
+    };
+    class LocalServerFileCont: RscButton
+    {
+    	idc = 1601;
+    	action = "copyToClipboard ""{	if (!isnull (getassignedcuratorunit _x)) then {		_unit = getassignedcuratorunit _x;		if (isnull (getassignedcuratorlogic _unit)) then {			unassignCurator _x;			sleep 1;			_unit assignCurator _x;			if (isClass (configFile >> 'CfgPatches' >> 'task_force_radio')) then {				_unit call TFAR_fnc_isForcedCurator;			};		};	};} foreach allcurators;""";
+
+    	text = "Copy File Contents"; //--- ToDo: Localize;
+    	x = 0.654726 * safezoneW + safezoneX;
+    	y = 0.401 * safezoneH + safezoneY;
     	w = 0.0722056 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -90,8 +125,8 @@ class VKN_Template_Tool_Info {
     	idc = 1002;
 
     	text = "initPlayerLocal.sqf"; //--- ToDo: Localize;
-    	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.401 * safezoneH + safezoneY;
+    	x = 0.577363 * safezoneW + safezoneX;
+    	y = 0.456 * safezoneH + safezoneY;
     	w = 0.0979933 * safezoneW;
     	h = 0.022 * safezoneH;
     };
@@ -102,7 +137,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Name"; //--- ToDo: Localize;
     	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.434 * safezoneH + safezoneY;
+    	y = 0.489 * safezoneH + safezoneY;
     	w = 0.067048 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -114,7 +149,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Contents"; //--- ToDo: Localize;
     	x = 0.654726 * safezoneW + safezoneX;
-    	y = 0.434 * safezoneH + safezoneY;
+    	y = 0.489 * safezoneH + safezoneY;
     	w = 0.0722056 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -125,7 +160,7 @@ class VKN_Template_Tool_Info {
 
     	text = "onPlayerRespawn.sqf"; //--- ToDo: Localize;
     	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.5 * safezoneH + safezoneY;
+    	y = 0.544 * safezoneH + safezoneY;
     	w = 0.0979933 * safezoneW;
     	h = 0.022 * safezoneH;
     };
@@ -136,7 +171,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Name"; //--- ToDo: Localize;
     	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.533 * safezoneH + safezoneY;
+    	y = 0.577 * safezoneH + safezoneY;
     	w = 0.067048 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -148,7 +183,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Contents"; //--- ToDo: Localize;
     	x = 0.654726 * safezoneW + safezoneX;
-    	y = 0.533 * safezoneH + safezoneY;
+    	y = 0.577 * safezoneH + safezoneY;
     	w = 0.0722056 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -159,7 +194,7 @@ class VKN_Template_Tool_Info {
 
     	text = "onPlayerKilled.sqf"; //--- ToDo: Localize;
     	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.599 * safezoneH + safezoneY;
+    	y = 0.632 * safezoneH + safezoneY;
     	w = 0.0979933 * safezoneW;
     	h = 0.022 * safezoneH;
     };
@@ -170,7 +205,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Name"; //--- ToDo: Localize;
     	x = 0.572206 * safezoneW + safezoneX;
-    	y = 0.632 * safezoneH + safezoneY;
+    	y = 0.665 * safezoneH + safezoneY;
     	w = 0.067048 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -182,7 +217,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Copy File Contents"; //--- ToDo: Localize;
     	x = 0.654726 * safezoneW + safezoneX;
-    	y = 0.632 * safezoneH + safezoneY;
+    	y = 0.665 * safezoneH + safezoneY;
     	w = 0.0722056 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.03,0.51,1,0.75};
@@ -194,7 +229,7 @@ class VKN_Template_Tool_Info {
 
     	text = "Got it!"; //--- ToDo: Localize;
     	x = 0.618623 * safezoneW + safezoneX;
-    	y = 0.698 * safezoneH + safezoneY;
+    	y = 0.72 * safezoneH + safezoneY;
     	w = 0.0515754 * safezoneW;
     	h = 0.033 * safezoneH;
     	colorActive[] = {0.298,0.949,0.251,0.75};
